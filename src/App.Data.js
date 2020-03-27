552961088f5ff4165afe930cb2c1c09e8a7228d9
Minhtho Tran <tran0202@gmail.com>
2020-03-27 17:25:46 -0500

    Add and update sport

---
+++ src/App.Data.js
@@ -0,0 +1,38 @@
+const AppData = {
+  collection: "sport",
+  batches: [
+    {
+      number: "1",
+      rows: [
+        {
+          id: "SOC",
+          data: {
+            name: "Soccer",
+            route: "/soccer"
+          }
+        }
+      ]
+    },
+    {
+        number: "2",
+        rows: [
+          {
+            id: "FBL",
+            data: {
+              name: "Football",
+              route: "/football"
+            }
+          },
+          {
+            id: "TEN",
+            data: {
+              name: "Tennis",
+              route: "/tennis"
+            }
+          }
+        ]
+      }
+  ]
+};
+
+export default AppData;
--- src/App.js
+++ src/App.js
@@ -1,102 +1,164 @@
-import React from 'react';
+import React from "react"
 
-import Firebase from 'firebase/app';
-import 'firebase/auth'
-import 'firebase/firestore'
-import config from './config';
-import logo from './logo.svg';
-import './App.css';
+import Firebase from "firebase/app"
+import "firebase/auth"
+import "firebase/firestore"
+import config from "./config"
+import logo from "./logo.svg"
+import "./App.css"
+import AppData from "./App.Data"
+import ReactJson from 'react-json-view'
+import { Button, Card, CardText } from "reactstrap"
 
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+    Firebase.initializeApp(config)
 
-  constructor(props){
-    super(props);
-    Firebase.initializeApp(config);
-
-    // this.state = {
-    //   developers: []
-    // }
-
-    this.state = {}
+    this.state = {
+      docs: [],
+      batchNumber: 0,
+      batchData: null,
+      staticStore: null
+    }
   }
-  
+
+  findUploadBatch = batchNumber => {
+    if (this.state.staticStore) {
+      const batches = this.state.staticStore.batches
+      const batchIndex = batches.findIndex(batch => {
+        return parseInt(batch.number, 10) === batchNumber
+      })
+      const batch = batches[batchIndex]
+      return batch
+    }
+  }
+
   getUserData = () => {
-    Firebase.firestore().collection('sport').get()
-    .then((snapshot) => {
-      snapshot.forEach((doc) => {
-        this.setState({id: doc.id, name: doc.data().name});
-        // console.log({id:doc.id,name:doc.data().name});
-        console.log(doc.id, '=>>>', doc.data());
-        // tmp = 'my-create-react-app 4';
-      });
-    })
-    .catch((err) => {
-      console.log('Error getting documents', err);
-    });
-    console.log('DATA RETRIEVED');
+    Firebase.firestore()
+      .collection("sport")
+      .get()
+      .then(snapshot => {
+        let tmp = []
+        snapshot.forEach(doc => {
+          tmp.push({ id: doc.id, data: doc.data() })
+        })
+        this.setState({ docs: tmp, staticStore: AppData })
+        window.sportStore = this.state
+        window.sportDataStore = AppData
+      })
+      .catch(err => {
+        console.log("Error getting documents", err)
+      })
   }
 
-  // window.firebase.firestore().collection('sport').get()
-  // .then((snapshot) => {
-  //   snapshot.forEach((sport) => {
-  //     console.log(sport.id, '=>>>', sport.data());
-  //     tmp = 'my-create-react-app 4';
-  //   });
-  // })
-  // .catch((err) => {
-  //   console.log('Error getting documents', err);
-  // });
+  writeUserData = () => {
+    const batch = this.findUploadBatch(this.state.batchNumber)
+    if (batch) {
+      batch.rows.map(row => {
+        const ts = Firebase.firestore.Timestamp.now()
+          .toDate()
+          .toLocaleString()
+        Firebase.firestore()
+          .collection("sport")
+          .doc(row.id)
+          .set({ ...row.data, timestamp: ts })
+        return true
+      })
+      this.getUserData()
+      this.setState({ batchNumber: 0 })
+    }
+  }
 
-  // getUserData = () => {
-  //   let ref = Firebase.database().ref('/');
-  //   ref.on('value', snapshot => {
-  //     const state = snapshot.val();
-  //     this.setState(state);
-  //   });
-  //   console.log('DATA RETRIEVED');
-  // }
+  showUploadBatch = event => {
+    event.preventDefault()
+    const batchInput = parseInt(this.refs.batch.value, 10)
+    if (this.refs.batch.value && batchInput > 0) {
+      const batch = this.findUploadBatch(batchInput)
+      this.setState({ batchData: batch })
+    }
+    else {
+      this.setState({ batchData: null })
+    }
+  }
 
   componentDidMount() {
-    this.getUserData();
+    this.getUserData()
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    // check on previous state
+    // only write when it's different with the new state
+    if (prevState.batchNumber !== this.state.batchNumber) {
+      this.writeUserData()
+    }
+  }
+
+  handleSubmit = event => {
+    event.preventDefault()
+    const batchInput = parseInt(this.refs.batch.value, 10)
+    const { docs } = this.state
+    if (this.refs.batch.value && batchInput > 0) {
+      this.setState({ docs, batchNumber: batchInput })
+    }
   }
 
   render() {
-    const { id, name } = this.state;
+    const { docs, batchData } = this.state
+    // console.log('batchData', batchData)
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <p>
-            id = {id} => name = {name}
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <p>Which sport are you interested in?</p>
+          {docs.map(doc => (
+            <p key={doc.id}>
+              {doc.id} => {doc.data.name} {doc.data.route} {doc.data.timestamp}
+            </p>
+          ))}
         </header>
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-12">
+              <h1>Add new sport below</h1>
+            </div>
+            <div className="col-sm-6">
+              <form onSubmit={this.handleSubmit}>
+                <div className="form-row">
+                  <input type="hidden" ref="id" />
+                  <div className="form-group col-md-6">
+                    <label>Batch </label>
+                    <input
+                      type="text"
+                      ref="batch"
+                      className="form-control"
+                      placeholder="Batch number"
+                      onChange={this.showUploadBatch}
+                    />
+                  </div>
+                </div>
+                <Button
+                  type="submit"
+                  className="btn btn-primary"
+                  color="primary"
+                >
+                  Add
+                </Button>
+              </form>
+            </div>
+            <div className="col-sm-6">
+              <label>To be added </label>
+              <Card body className="text-left">
+                <CardText>
+                  { batchData && <ReactJson src={batchData}/>}
+                </CardText>
+              </Card>
+            </div>
+          </div>
+        </div>
       </div>
-    );
+    )
   }
-
-  // writeUserData = () => {
-  //   Firebase.database().ref('/').set(this.state);
-  //   console.log('DATA SAVED');
-  // }
-  
-  // componentDidUpdate(prevProps, prevState) {
-  //   // check on previous state
-  //   // only write when it's different with the new state
-  //   if (prevState !== this.state) {
-  //     this.writeUserData();
-  //   }
-  // }
 
   // render() {
   //   const { developers } = this.state;
@@ -109,9 +171,9 @@ class App extends React.Component {
   //       </div>
   //       <div className='row'>
   //         <div className='col-xl-12'>
-  //         { 
+  //         {
   //           developers
-  //           .map(developer => 
+  //           .map(developer =>
   //             <div key={developer.uid} className="card float-left" style={{width: '18rem', marginRight: '1rem'}}>
   //               <div className="card-body">
   //                 <h5 className="card-title">{ developer.name }</h5>
@@ -121,7 +183,7 @@ class App extends React.Component {
   //               </div>
   //             </div>
   //             )
-  //         } 
+  //         }
   //         </div>
   //       </div>
   //       <div className='row'>
@@ -147,33 +209,6 @@ class App extends React.Component {
   //   )
   // }
 
-  // handleSubmit = (event) => {
-  //   event.preventDefault();
-  //   let name = this.refs.name.value;
-  //   let role = this.refs.role.value;
-  //   let uid = this.refs.uid.value;
-  
-  //   if (uid && name && role){
-  //     const { developers } = this.state;
-  //     const devIndex = developers.findIndex(data => {
-  //       return data.uid === uid 
-  //     });
-  //     developers[devIndex].name = name;
-  //     developers[devIndex].role = role;
-  //     this.setState({ developers });
-  //   }
-  //   else if (name && role ) {
-  //     const uid = new Date().getTime().toString();
-  //     const { developers } = this.state;
-  //     developers.push({ uid, name, role })
-  //     this.setState({ developers });
-  //   }
-  
-  //   this.refs.name.value = '';
-  //   this.refs.role.value = '';
-  //   this.refs.uid.value = '';
-  // }
-  
   // removeData = (developer) => {
   //   const { developers } = this.state;
   //   const newState = developers.filter(data => {
@@ -188,26 +223,5 @@ class App extends React.Component {
   //   this.refs.role.value = developer.role;
   // }
 }
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 
 export default App
