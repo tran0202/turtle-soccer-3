08df28e30f419b68f8d01f06285751717d3ba811
Minhtho Tran <tran0202@gmail.com>
2020-04-02 17:06:47 -0500

    Favicon and Logo

--- src/core/Page.js
+++ src/core/Page.js
@@ -1,14 +1,14 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./Page.css";
-import logo from "../assets/logo.svg";
+import turtleLogo from "../assets/LogoMakr_1aN4h4.png";
 
 class Page extends React.Component {
     render() {
         return (
             <div className="Page">
                 <header className="Page-header">
-                    <img src={logo} className="Page-logo" alt="logo" />
+                    <img src={turtleLogo} alt="Turtle logo" />
                 </header>
                 {this.props.children}
             </div>
