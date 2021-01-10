06c6c833ce8792807916eb86629c023144b58426
Minhtho Tran <tran0202@gmail.com>
2021-01-09 20:23:11 -0600

    Men's Olympic Football Tournament Barcelona 1992

--- src/soccer/DataHelper.js
+++ src/soccer/DataHelper.js
@@ -41,6 +41,7 @@ import td_EURO_2008 from '../data/soccer/tournamentData/EURO/td_EURO_2008.json'
 import td_EURO_2012 from '../data/soccer/tournamentData/EURO/td_EURO_2012.json'
 import td_EURO_2016 from '../data/soccer/tournamentData/EURO/td_EURO_2016.json'
 
+import td_MOFT_1992 from '../data/soccer/tournamentData/MOFT/td_MOFT_1992.json'
 import td_MOFT_1996 from '../data/soccer/tournamentData/MOFT/td_MOFT_1996.json'
 import td_MOFT_2000 from '../data/soccer/tournamentData/MOFT/td_MOFT_2000.json'
 import td_MOFT_2004 from '../data/soccer/tournamentData/MOFT/td_MOFT_2004.json'
@@ -52,7 +53,7 @@ import QualificationTournamentWCArray from '../data/soccer/QualificationTourname
 import qtd_WC_2022 from '../data/soccer/qualTournamentData/qtd_WC_2022.json'
 
 export const getCurrentTournament = () => {
-  return { tournament: 'MOFT1992', qualificationTournament: 'WC2022_CONMEBOL' }
+  return { tournament: 'MOFT1988', qualificationTournament: 'WC2022_CONMEBOL' }
 }
 
 export const getTournamentArray = () => {
@@ -64,7 +65,7 @@ export const getTournamentDataArray = () => {
 }
 
 export const getTournamentDataMOFT = () => {
-  return [].concat([td_MOFT_1996], [td_MOFT_2000], [td_MOFT_2004], [td_MOFT_2008], [td_MOFT_2012], [td_MOFT_2016])
+  return [].concat([td_MOFT_1992], [td_MOFT_1996], [td_MOFT_2000], [td_MOFT_2004], [td_MOFT_2008], [td_MOFT_2012], [td_MOFT_2016])
 }
 
 export const getQualificationTournamentArray = () => {
