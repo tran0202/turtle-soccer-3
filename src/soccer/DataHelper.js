09d2d57071fbd9423a036ffbd21a69ecd57b352c
Minhtho Tran <tran0202@gmail.com>
2021-01-26 20:51:19 -0600

    South American Championship Ecuador 1959

--- src/soccer/DataHelper.js
+++ src/soccer/DataHelper.js
@@ -75,6 +75,7 @@ import td_WOFT_2008 from '../data/soccer/tournamentData/WOFT/td_WOFT_2008.json'
 import td_WOFT_2012 from '../data/soccer/tournamentData/WOFT/td_WOFT_2012.json'
 import td_WOFT_2016 from '../data/soccer/tournamentData/WOFT/td_WOFT_2016.json'
 
+import td_COPA_1959ECU from '../data/soccer/tournamentData/COPA/td_COPA_1959ECU.json'
 import td_COPA_1963 from '../data/soccer/tournamentData/COPA/td_COPA_1963.json'
 import td_COPA_1967 from '../data/soccer/tournamentData/COPA/td_COPA_1967.json'
 import td_COPA_1975 from '../data/soccer/tournamentData/COPA/td_COPA_1975.json'
@@ -99,7 +100,7 @@ import QualificationTournamentWCArray from '../data/soccer/QualificationTourname
 import qtd_WC_2022 from '../data/soccer/qualTournamentData/qtd_WC_2022.json'
 
 export const getCurrentTournament = () => {
-  return { tournament: 'COPA1959ECU', qualificationTournament: 'WC2022_CONMEBOL' }
+  return { tournament: 'COPA1959ARG', qualificationTournament: 'WC2022_CONMEBOL' }
 }
 
 export const getTournamentArray = () => {
@@ -112,6 +113,7 @@ export const getTournamentDataArray = () => {
 
 export const getTournamentDataCOPA = () => {
   return [].concat(
+    [td_COPA_1959ECU],
     [td_COPA_1963],
     [td_COPA_1967],
     [td_COPA_1975],
