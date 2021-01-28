d7ad883a7a4b51cba529db2de8eb87ba47a36f7d
Minhtho Tran <tran0202@gmail.com>
2021-01-28 07:26:00 -0600

    South American Championship Peru 1927

--- src/soccer/DataHelper.js
+++ src/soccer/DataHelper.js
@@ -75,6 +75,7 @@ import td_WOFT_2008 from '../data/soccer/tournamentData/WOFT/td_WOFT_2008.json'
 import td_WOFT_2012 from '../data/soccer/tournamentData/WOFT/td_WOFT_2012.json'
 import td_WOFT_2016 from '../data/soccer/tournamentData/WOFT/td_WOFT_2016.json'
 
+import td_COPA_1927 from '../data/soccer/tournamentData/COPA/td_COPA_1927.json'
 import td_COPA_1929 from '../data/soccer/tournamentData/COPA/td_COPA_1929.json'
 import td_COPA_1935 from '../data/soccer/tournamentData/COPA/td_COPA_1935.json'
 import td_COPA_1937 from '../data/soccer/tournamentData/COPA/td_COPA_1937.json'
@@ -115,7 +116,7 @@ import QualificationTournamentWCArray from '../data/soccer/QualificationTourname
 import qtd_WC_2022 from '../data/soccer/qualTournamentData/qtd_WC_2022.json'
 
 export const getCurrentTournament = () => {
-  return { tournament: 'COPA1927', qualificationTournament: 'WC2022_CONMEBOL' }
+  return { tournament: 'COPA1926', qualificationTournament: 'WC2022_CONMEBOL' }
 }
 
 export const getTournamentArray = () => {
@@ -128,6 +129,7 @@ export const getTournamentDataArray = () => {
 
 export const getTournamentDataCOPA = () => {
   return [].concat(
+    [td_COPA_1927],
     [td_COPA_1929],
     [td_COPA_1935],
     [td_COPA_1937],
