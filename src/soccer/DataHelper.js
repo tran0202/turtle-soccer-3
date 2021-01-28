8d77bc0dc1773a7bcfe4b6e0cc47ecbd328e6cab
Minhtho Tran <tran0202@gmail.com>
2021-01-28 15:16:51 -0600

    South American Championship Chile 1920

--- src/soccer/DataHelper.js
+++ src/soccer/DataHelper.js
@@ -75,6 +75,7 @@ import td_WOFT_2008 from '../data/soccer/tournamentData/WOFT/td_WOFT_2008.json'
 import td_WOFT_2012 from '../data/soccer/tournamentData/WOFT/td_WOFT_2012.json'
 import td_WOFT_2016 from '../data/soccer/tournamentData/WOFT/td_WOFT_2016.json'
 
+import td_COPA_1920 from '../data/soccer/tournamentData/COPA/td_COPA_1920.json'
 import td_COPA_1921 from '../data/soccer/tournamentData/COPA/td_COPA_1921.json'
 import td_COPA_1922 from '../data/soccer/tournamentData/COPA/td_COPA_1922.json'
 import td_COPA_1923 from '../data/soccer/tournamentData/COPA/td_COPA_1923.json'
@@ -122,7 +123,7 @@ import QualificationTournamentWCArray from '../data/soccer/QualificationTourname
 import qtd_WC_2022 from '../data/soccer/qualTournamentData/qtd_WC_2022.json'
 
 export const getCurrentTournament = () => {
-  return { tournament: 'COPA1920', qualificationTournament: 'WC2022_CONMEBOL' }
+  return { tournament: 'COPA1919', qualificationTournament: 'WC2022_CONMEBOL' }
 }
 
 export const getTournamentArray = () => {
@@ -135,6 +136,7 @@ export const getTournamentDataArray = () => {
 
 export const getTournamentDataCOPA = () => {
   return [].concat(
+    [td_COPA_1920],
     [td_COPA_1921],
     [td_COPA_1922],
     [td_COPA_1923],
