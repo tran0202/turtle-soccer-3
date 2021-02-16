1aa55af6f364daf265114bee3e95d6ac8d569fe1
Minhtho Tran <tran0202@gmail.com>
2021-02-15 23:11:28 -0600

    AFC Asian Cup Singapore 1984

--- src/soccer/DataHelper.js
+++ src/soccer/DataHelper.js
@@ -184,6 +184,7 @@ import td_AFCON_2015 from '../data/soccer/tournamentData/AFCON/td_AFCON_2015.jso
 import td_AFCON_2017 from '../data/soccer/tournamentData/AFCON/td_AFCON_2017.json'
 import td_AFCON_2019 from '../data/soccer/tournamentData/AFCON/td_AFCON_2019.json'
 
+import td_ACC_1984 from '../data/soccer/tournamentData/AAC/td_AAC_1984.json'
 import td_ACC_1988 from '../data/soccer/tournamentData/AAC/td_AAC_1988.json'
 import td_ACC_1992 from '../data/soccer/tournamentData/AAC/td_AAC_1992.json'
 import td_ACC_1996 from '../data/soccer/tournamentData/AAC/td_AAC_1996.json'
@@ -198,7 +199,7 @@ import QualificationTournamentWCArray from '../data/soccer/QualificationTourname
 import qtd_WC_2022 from '../data/soccer/qualTournamentData/qtd_WC_2022.json'
 
 export const getCurrentTournament = () => {
-  return { tournament: 'AAC1984', qualificationTournament: 'WC2022_CONMEBOL' }
+  return { tournament: 'AAC1980', qualificationTournament: 'WC2022_CONMEBOL' }
 }
 
 export const getTournamentArray = () => {
@@ -219,7 +220,18 @@ export const getTournamentDataArray = () => {
 }
 
 export const getTournamentDataAAC = () => {
-  return [].concat([td_ACC_1988], [td_ACC_1992], [td_ACC_1996], [td_ACC_2000], [td_ACC_2004], [td_ACC_2007], [td_ACC_2011], [td_ACC_2015], [td_ACC_2019])
+  return [].concat(
+    [td_ACC_1984],
+    [td_ACC_1988],
+    [td_ACC_1992],
+    [td_ACC_1996],
+    [td_ACC_2000],
+    [td_ACC_2004],
+    [td_ACC_2007],
+    [td_ACC_2011],
+    [td_ACC_2015],
+    [td_ACC_2019],
+  )
 }
 
 export const getQualificationTournamentArray = () => {
