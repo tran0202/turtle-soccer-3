4b2d124727409d1d66e08c6c8b9b9dc31688abde
Minhtho Tran <tran0202@gmail.com>
2021-02-18 21:27:41 -0600

    Women's World Cup USA 2003

--- src/soccer/DataHelper.js
+++ src/soccer/DataHelper.js
@@ -215,6 +215,7 @@ import td_ONC_2008 from '../data/soccer/tournamentData/ONC/td_ONC_2008.json'
 import td_ONC_2012 from '../data/soccer/tournamentData/ONC/td_ONC_2012.json'
 import td_ONC_2016 from '../data/soccer/tournamentData/ONC/td_ONC_2016.json'
 
+import td_WWC_2003 from '../data/soccer/tournamentData/WWC/td_WWC_2003.json'
 import td_WWC_2007 from '../data/soccer/tournamentData/WWC/td_WWC_2007.json'
 import td_WWC_2011 from '../data/soccer/tournamentData/WWC/td_WWC_2011.json'
 import td_WWC_2015 from '../data/soccer/tournamentData/WWC/td_WWC_2015.json'
@@ -224,7 +225,7 @@ import QualificationTournamentWCArray from '../data/soccer/QualificationTourname
 import qtd_WC_2022 from '../data/soccer/qualTournamentData/qtd_WC_2022.json'
 
 export const getCurrentTournament = () => {
-  return { tournament: 'WWC2003', qualificationTournament: 'WC2022_CONMEBOL' }
+  return { tournament: 'WWC1999', qualificationTournament: 'WC2022_CONMEBOL' }
 }
 
 export const getTournamentArray = () => {
@@ -247,7 +248,7 @@ export const getTournamentDataArray = () => {
 }
 
 export const getTournamentDataWWC = () => {
-  return [].concat([td_WWC_2007], [td_WWC_2011], [td_WWC_2015], [td_WWC_2019])
+  return [].concat([td_WWC_2003], [td_WWC_2007], [td_WWC_2011], [td_WWC_2015], [td_WWC_2019])
 }
 
 export const getQualificationTournamentArray = () => {
