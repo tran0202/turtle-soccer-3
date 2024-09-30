d3c3c447f65e6c155355a12ec260885665397567
Minhtho Tran <tran0202@gmail.com>
2024-09-30 17:52:19 -0400

    Start fresh

--- src/App.js
+++ src/App.js
@@ -1,102 +1,25 @@
-import React from 'react'
-import Page from './core/Page'
-import { Container } from 'reactstrap'
+import logo from './logo.svg';
+import './App.css';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props)
-    document.title = 'Turtle Soccer 3'
-  }
-
-  render() {
-    return (
-      <Page>
-        <Container>
-          <section id="hero">
-            <div className="hero-container">
-              <div id="heroCarousel" className="carousel slide carousel-fade" data-ride="carousel">
-                <ol className="carousel-indicators" id="hero-carousel-indicators"></ol>
-
-                <div className="carousel-inner" role="listbox">
-                  <div
-                    className="carousel-item active"
-                    style={{
-                      backgroundImage: "url('/assets/images/slide/soccer-ts1475731972.jpg')",
-                    }}
-                  >
-                    <div className="carousel-container">
-                      <div className="carousel-content container">
-                        <h2 className="animated fadeInDown h2-ff8">
-                          Welcome to <span>Turtle Soccer</span>
-                        </h2>
-                        <p className="animated fadeInUp">It's known as football for the rest of the world. In America, we call it Soccer!</p>
-                        <a href="/soccer" className="btn-get-started animated fadeInUp scrollto">
-                          Soccer
-                        </a>
-                      </div>
-                    </div>
-                  </div>
-
-                  <div
-                    className="carousel-item"
-                    style={{
-                      backgroundImage: "url('/assets/images/slide/football-557206_1920.jpg')",
-                    }}
-                  >
-                    <div className="carousel-container">
-                      <div className="carousel-content container">
-                        <h2 className="animated fadeInDown h2-ff8">
-                          Welcome to <span>Turtle Football</span>
-                        </h2>
-                        <p className="animated fadeInUp">
-                          Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos
-                          quisquam cupiditate. Et nemo qui impedit suscipit alias ea.
-                        </p>
-                        <a href="/football" className="btn-get-started animated fadeInUp scrollto">
-                          Football
-                        </a>
-                      </div>
-                    </div>
-                  </div>
-
-                  <div
-                    className="carousel-item"
-                    style={{
-                      backgroundImage: "url('/assets/images/slide/tennis-fun-1-1398467.jpg')",
-                    }}
-                  >
-                    <div className="carousel-container">
-                      <div className="carousel-content container">
-                        <h2 className="animated fadeInDown h2-ff8">
-                          Welcome to <span>Turtle Tennis</span>
-                        </h2>
-                        <p className="animated fadeInUp">
-                          Ut velit est quam dolor ad a aliquid qui aliquid. Sequi ea ut et est quaerat sequi nihil ut aliquam. Occaecati alias dolorem mollitia
-                          ut. Similique ea voluptatem. Esse doloremque accusamus repellendus deleniti vel. Minus et tempore modi architecto.
-                        </p>
-                        <a href="/tennis" className="btn-get-started animated fadeInUp scrollto">
-                          Tennis
-                        </a>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-
-                <a className="carousel-control-prev" href="#heroCarousel" role="button" data-slide="prev">
-                  <span className="carousel-control-prev-icon icofont-rounded-left" aria-hidden="true"></span>
-                  <span className="sr-only">Previous</span>
-                </a>
-                <a className="carousel-control-next" href="#heroCarousel" role="button" data-slide="next">
-                  <span className="carousel-control-next-icon icofont-rounded-right" aria-hidden="true"></span>
-                  <span className="sr-only">Next</span>
-                </a>
-              </div>
-            </div>
-          </section>
-        </Container>
-      </Page>
-    )
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/firestore/team/Team.Data.4.js
+++
@@ -1,9 +0,0 @@
-export const AppData = {
-  collection: 'team',
-  batches: [
-    {
-      number: '11',
-      rows: [],
-    },
-  ],
-}
